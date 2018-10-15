1f993f5fa3361901486eb749844665f6eabd65ab
ajb871 <ajb871@nyu.edu>
2018-10-15 15:24:48 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  consumer_key:         'YOURKEY',
-  consumer_secret:      'YOUR SECRET',
-  access_token:         'YOUR TOKEN',
-  access_token_secret:  'YOUR TOKEN SECRET',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            true,     // optional - requires SSL certificates to be valid.
-}
